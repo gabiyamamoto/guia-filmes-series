cc8aad0d31c9ec226ceecf736ed7bc8ee2b7e3e7
gabi emi <gabiemi2021@gmail.com>
2026-04-19 18:02:00 -0300

    feat: começa a fazer tela inicial

--- App.js
+++ App.js
@@ -1,20 +1,106 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Ionicons } from '@expo/vector-icons';
+
+import InicioScreen from './src/screens/InicioScreen';
+import ListarFilmesScreen from './src/screens/ListaFilmesScreen'
+import ListarSeriesScreen from './src/screens/ListaSeriesScreen'
+import DetalhesScreen from './src/screens/DetalhesScreen'
+import SobreScreen from './src/screens/SobreScreen'
+import ContatoScreen from './src/screens/ContatoScreen'
+
+import { StackScreen } from 'react-native-screens';
+
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function FilmesStack() {
+  return (
+    <Stack.Navigator screenOptions={{
+      headerStyle: { backgroundColor: '#141414' },
+      headerTintColor: '#fff',
+      headerTitleStyle: { color: '#fff' },
+    }}>
+      <Stack.Screen name='ListaFilmes' component={ListaFilmesScreen} options={{ title: 'Filmes' }}>
+      </Stack.Screen>
+    </Stack.Navigator>
+  )
+}
+
+function TabNavigator() {
+  return (
+    <TabNavigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Filmes') {
+            iconName = focused ? 'film' : 'film-outline';
+          } else if (route.name === 'Séries') {
+            iconName = focused ? 'tv' : 'tv-outline';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#e50914',
+        tabBarInactiveTintColor: 'gray',
+        headerShown: false
+      })}>
+      <Tab.Screen name='Filmes' component={ListarFilmesScreen} />
+      <Tab.Screen name='Séries' component={ListarSeriesScreen} />
+    </TabNavigator >
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator
+        screenOptions={{
+          drawerStyle: {
+            backgroundColor: '#6d6969',
+            width: 280,
+          },
+          drawerLabelStyle: {
+            color: '#fff',
+            fontSize: 16,
+          },
+          drawerActiveTintColor: '#e50914',
+          drawerInactiveTintColor: '#ccc',
+          headerStyle: {
+            backgroundColor: '#141414',
+          },
+          headerTintColor: '#fff',
+        }}
+      >
+        <Drawer.Screen
+          name='Início'
+          component={TabNavigator}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="home-outline" size={size} color={color} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name='Sobre'
+          component={SobreScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="information-circle-outline" size={size} color={color} />
+            ),
+          }} />
+        <Drawer.Screen
+          name='Contato'
+          component={ContatoScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="mail-outline" size={size} color={color} />
+            ),
+          }}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
